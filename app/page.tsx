b122d4b1dadd14c2b4997f9503a5a0ca710319f0
Smesj <smeschishnick@gmail.com>
2024-02-06 10:47:14 -0600

    add clerk auth

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 'use client'
+import {  UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 
@@ -13,16 +14,7 @@ export default function Home() {
 					<code className="font-mono font-bold">Smesj World</code>
 				</p>
 				<div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-					<a
-						className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-						target="_blank"
-						rel="noopener noreferrer"
-						onClick={() => {
-							router.push('/login');
-						}}
-					>
-						By smesj.exe
-					</a>
+					<UserButton />
 				</div>
 			</div>
 
